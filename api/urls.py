928fe9da37ea420acfa967784892c8ac402dc0b3
rayvikram <vik8876@gmail.com>
2020-09-27 23:54:07 +0530

    Viewset added in url file

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
+
+from .views import Connect4ViewSet
 
 router = routers.DefaultRouter()
 # register viewsets of api app here
 
+router.register("", Connect4ViewSet, basename="connect")
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
